46b02dae2e28e26956724f614e27cf66d42685f0
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-16 09:27:52 -0300

    092716042022

---
+++ 008Propiedades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _008Propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**/
+        }
+
+        public class Pagamentos
+        {
+            //Uma propiedade
+            public DateTime Vencimento { get; set; }
+
+            /*Para ler um valor passamos pelo metodo get;
+              Para atribuir um valor, usamos  metodo set;*/
+
+            private int _numero;
+
+            public int NumeroPagamento
+            {
+                //Lendo o valor;
+                get { return _numero; }
+
+                //Atribuindo o valor
+                set { _numero = value; }
+            }
+
+
+            public void Pagar()
+            {
+
+            }
+        }
+    }
+}
